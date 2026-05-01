b18e12473b748d9fbe770de535700b3de6fffdc8
Graffian <rudrabehera2006@gmail.com>
2026-05-01 15:03:03 +0530

    extracted content length

--- backend/server.c
+++ backend/server.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <winsock2.h>
-
+#include <string.h>
 
 int main(){
     char buffer[100];
+    int content_length;
     char response[] =
     "HTTP/1.1 200 OK\r\n"
     "Access-Control-Allow-Origin: *\r\n"
     "Content-Type: text/plain\r\n"
     "\r\n"
     "Hello";
-    FILE *fp = fopen("../storage/test.txt" , "wb");
     WSADATA wsa;
     WSAStartup(MAKEWORD(2,2) , &wsa);
     SOCKET client_socket , server_socket;
@@ -22,18 +22,18 @@ int main(){
     bind(server_socket , (struct sockaddr *)&server , sizeof(server));
     printf("STARTING SERVER........\n");
     listen(server_socket , 3);
-    printf("SERVER LISTENING TO PORT 8080");
+    printf("SERVER LISTENING TO PORT 8080\n");
     int c = sizeof(struct sockaddr_in);
     while (1){
         client_socket = accept(server_socket , (struct sockaddr *)&client , &c);
         int bytes = recv(client_socket , buffer , sizeof(buffer)-1 , 0);
         buffer[bytes] = '\0';
-        printf("Client said : %s\n" , buffer);
-        fwrite(buffer , 1 , sizeof(buffer) , fp);
-        fclose(fp);
-        send(client_socket , response , sizeof(response) , 0);
+        char *p = strstr(buffer , "Content-Length:");
+        p+=15;
+        content_length = atoi(p);
+        printf("%d" , content_length);
+        send(client_socket , response , strlen(response) , 0);
         closesocket(client_socket);
-
     }
     closesocket(server_socket);
     WSACleanup();
